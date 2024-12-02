22b6ec88d15a934c3edd78a63ff82e290b8ad562
SneakyPizza <jinxie88@live.nl>
2024-12-02 21:16:20 +0100

    --- Day 1: Historian Hysteria --- --- Part Two ---

--- day1.py
+++ day1.py
@@ -13,7 +13,17 @@ right.sort(reverse=False)
 total = 0
 for i in range(len(left)):
     value = (max(left[i], right[i]) - min(left[i],right[i]))
-    print(f'distance between {left[i]} and {right[i]} equals {value}')
+    # print(f'distance between {left[i]} and {right[i]} equals {value}')
     total += value
 
 print(f'total value = {total}')
+
+simscore = 0 
+for i in range(len(left)):
+    appeared = 0
+    for j in range(len(right)):
+        if(left[i] == right[j]):
+            appeared += 1
+    simscore += (appeared * left[i])
+
+print(f'Similarity score: {simscore}')
